0d7c8ec54b034fa11ca3d318d3e61f348c2eddb0
DreaconWalker <samarshi19@gmail.com>
2021-10-27 10:46:21 +0530

    added firebase guest login

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -20,19 +20,21 @@ class _SignInState extends State<SignIn> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          ElevatedButton(
-              onPressed: () async {
-                dynamic result = await _authService.signInAnony();
-                if (result != null) {
-                  print('error signed in');
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => HomePage(title: 'Dice Roller'),
-                    ),
-                  );
-                }
-              },
-              child: Text('Sign in as guest'))
+          Center(
+            child: ElevatedButton(
+                onPressed: () async {
+                  dynamic result = await _authService.signInAnony();
+                  if (result != null) {
+                    print('error signed in');
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(title: 'Dice Roller'),
+                      ),
+                    );
+                  }
+                },
+                child: Text('Sign in as guest')),
+          )
         ],
       ),
     );
